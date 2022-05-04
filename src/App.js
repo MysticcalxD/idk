9692d81c88490224e9c18066847d45ce3a06c26d
UrNotGonnaKnow <mariafsyeda@gmail.com>
2022-05-04 09:03:41 -0400

    yea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
@@ -9,9 +8,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>U suck</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/search?q=LEARN+IT+YOURSELF%21%21%21&ei=0xJxYsHCMo3dtQa2x4LYDQ&ved=0ahUKEwiB-KLsncP3AhWNbs0KHbajANsQ4dUDCA4&uact=5&oq=LEARN+IT+YOURSELF%21%21%21&gs_lcp=Cgdnd3Mtd2l6EAMyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoQCC4QsQMQgwEQxwEQ0QMQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwgAEIAEELEDEIMBOgUIABCABDoRCC4QgAQQsQMQgwEQxwEQowI6BAgAEEM6BwguENQCEEM6DgguEIAEELEDEMcBEKMCOggIABCABBCxAzoOCC4QgAQQsQMQxwEQ0QM6CwguEIAEELEDENQCOggILhCABBCxAzoICAAQgAQQyQM6BQgAEJIDOgUIABCRAjoICAAQsQMQkQI6BQguEIAEOgcIABCxAxBDOhEILhCxAxCDARDHARDRAxCRAjoLCC4QgAQQsQMQgwE6FAguEIAEELEDEIMBEMcBEKMCENQCOggIABCxAxCDAToECC4QQzoNCC4QxwEQowIQ1AIQQzoFCAAQsQNKBAhBGABKBAhGGABQAFjlb2DDeWgKcAF4AIABmQGIAcsXkgEFMTguMTKYAQCgAQGwAQDAAQE&sclient=gws-wiz"
           target="_blank"
           rel="noopener noreferrer"
         >
